850928aad0c98a9e11504f391df6f3903b6454db
Sam <mastersamdev@gmail.com>
2026-02-26 00:58:35 +0500

    hello

--- src/components/ThreeDViewer.jsx
+++ src/components/ThreeDViewer.jsx
@@ -22,7 +22,7 @@ function Loader() {
 
 // --- 2. University Model Loader ---
 function UniversityModel({ setModelScene }) {
-  const { scene } = useGLTF('https://docs.google.com/uc?export=download&id=1dIOvnco9Fa9fN1InHI7gJH-7vXK8_jyH')
+  const { scene } = useGLTF('./Final121.glb')
   useEffect(() => { 
     if (scene) {
       scene.updateMatrixWorld(true)
